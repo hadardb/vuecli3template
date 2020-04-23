7b2381108296a908e40dc3086ffd03ff6f296158
sunhaojin <378248936@qq.com>
2020-04-23 14:38:40 +0800

    feat: 🎸 添加commitizen插件 添加element框架 完善eslint检测

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,10 @@
+<!--
+ * @name: 文件
+ * @Author: Haojin Sun
+ * @Date: 2019-12-02 12:15:20
+ * @LastEditors: Haojin Sun
+ * @LastEditTime: 2020-04-23 14:38:17
+ -->
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
@@ -8,7 +15,6 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
   components: {
